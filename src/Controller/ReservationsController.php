1f6ac832159e53e5a89a0212e5d3d9ea63a00f46
Sebastien Guerin <sebast.guerin@gmail.com>
2019-04-11 15:37:29 +0200

    Début d'intégration formulaire réservation

--- src/Controller/ReservationsController.php
+++ src/Controller/ReservationsController.php
@@ -3,7 +3,26 @@
 
 namespace App\Controller;
 
+use App\Model\ReservationsManager;
+
 class ReservationsController extends AbstractController
 {
+    public function reserver()
+    {
+        return $this->twig->render('Reservations/reserver.html.twig');
+    }
 
+    public function list()
+    {
+        $resaManager = new ReservationsManager();
+        $reservations = $resaManager->selectAll();
+        return $this->twig->render('Reservations/list.html.twig', ['reservations' => $reservations]);
+    }
+
+    public function show(int $id)
+    {
+        $resaManager = new ReservationsManager();
+        $reservation =  $resaManager->selectOneById($id);
+        return $this->twig->render('Reservations/show.html.twig', ['reservation' => $reservation]);
+    }
 }
--- src/Model/ReservationsManager.php
+++ src/Model/ReservationsManager.php
@@ -6,4 +6,53 @@ namespace App\Model;
 class ReservationsManager extends AbstractManager
 {
 
+    const TABLE = 'reservations';
+
+    /**
+     *  Initializes this class.
+     */
+    public function __construct()
+    {
+        parent::__construct(self::TABLE);
+    }
+
+    public function insert(array $reservation): int
+    {
+        // prepared request
+        $statement = $this->pdo->prepare("INSERT INTO $this->table (`title`) VALUES (:title)");
+        $statement->bindValue('title', $reservation['title'], \PDO::PARAM_STR);
+
+        if ($statement->execute()) {
+            return (int)$this->pdo->lastInsertId();
+        }
+    }
+
+
+    // Actions admin
+
+    /**
+     * @param int $id
+     */
+    public function decline(int $id): void
+    {
+        // prepared request
+        $statement = $this->pdo->prepare("DELETE FROM $this->table WHERE id=:id");
+        $statement->bindValue('id', $id, \PDO::PARAM_INT);
+        $statement->execute();
+    }
+
+
+    /**
+     * @param array $reservation
+     * @return bool
+     */
+    public function confirm(array $reservation):bool
+    {
+        // prepared request
+        $statement = $this->pdo->prepare("UPDATE $this->table SET `title` = :title WHERE id=:id");
+        $statement->bindValue('id', $reservation['id'], \PDO::PARAM_INT);
+        $statement->bindValue('title', $reservation['title'], \PDO::PARAM_STR);
+
+        return $statement->execute();
+    }
 }
