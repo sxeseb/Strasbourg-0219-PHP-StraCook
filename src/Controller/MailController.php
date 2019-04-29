a629ab341edaa43ddc9e5bfb9bbd57790416b120
Adeline Jacquot <ade.jacquot@gmail.com>
2019-04-29 09:44:50 +0200

    newsletter controlelr

--- src/Controller/MailController.php
+++ src/Controller/MailController.php
@@ -16,23 +16,35 @@ class MailController extends AbstractController
 
     public function newsletter()
     {
-        $mails ='';
-
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $validator = new MailController();
-            $output = $validator->mail();
-            list($errors, $datas) = $output;
-            if (!empty($errors)) {
-                return $this->twig->render(
-                    'Home/index.html.twig',
-                    ['mail' => $mails, 'errors' => $errors, 'datas' => $datas]
-                );
-            } else {
-                // insertion mail
-
-                $mails = '';
+        if(isset($_POST['newsletterform'])){
+            if(isset($_POST['newsletter'])){
+                if(!empty($_POST['newsletter'])){
+                    $newsletter = htmlspecialchars($_POST['newsletter']);
+                    if(filter_var($newsletter, FILTER_VALIDATE_EMAIL)) {
+                        $reqip = $bdd->prepare("SELECT * FROM newsletter WHERE ip = ?");
+                        $reqip->execute(array($_SERVER['REMOTE_ADDR']));
+                        $ipexist = $reqip->rowCount();
+                        if($ipexist == 0) {
+                            $reqmail = $bdd->prepare("SELECT * FROM newsletter WHERE email = ?");
+                            $reqmail->execute(array($newsletter));
+                            $mailexist = $reqmail->rowCount();
+                            if($mailexist == 0){
+                                $sql = $bdd->prepare('INSERT INTO newsletter(email,ip,dates) VALUES (?,?,NOW())');
+                                $sql->execute(array($newsletter,$_SERVER['REMOTE_ADDR']));
+                                header("Location: newsletter.php");
+                            } else {
+                                $erreur = "Vous êtes déjà inscrit à la Newsletter..";
+                            }
+                        } else {
+                            $erreur = "Vous êtes déjà inscrit à la Newsletter..";
+                        }
+                    } else {
+                        $erreur = "Vous devez indiquer une adresse e-mail..";
+                    }
+                } else {
+                    $erreur = "Vous devez remplir tout les champs vides..";
+                }
             }
         }
-        return $this->twig->render('Home/index.html.twig', ['mail' => $mails]);
     }
 }
